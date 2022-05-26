88f8b33236e02ed27d9ab2db5f0129cbc054e3c5
NotEvil <worldwow225@gmail.com>
2022-05-26 18:26:21 +0400

    server update

--- index.js
+++ index.js
@@ -1,66 +1,100 @@
-const path = require('path')
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'), { foreginKeySuffix: 'id' })
-const middlewares = jsonServer.defaults({ bodyParser: true })
-const constants = require('./constants')
+const path = require('path');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'), { foreginKeySuffix: 'id' });
+const middlewares = jsonServer.defaults({ bodyParser: true });
+const constants = require('./constants');
 
+server.db = router.db;
 
-server.db = router.db
+server.use(jsonServer.bodyParser);
 
-server.use(jsonServer.bodyParser)
+server.post('/registration', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
 
-server.post('/registration', (req, res, next) => {
-  const { db } = req.app
-  const {password, email} = req.body
-  const isSameMail = db.get('users').find({email}).value()
+  const { db } = req.app;
+  const { password, email } = req.body;
+  const isSameMail = db.get('users').find({ email }).value();
 
-  if(isSameMail) {
-    return res.status(200).jsonp({mailMessage: 'Email is already in use'})
+  if (isSameMail) {
+    return res.status(200).jsonp({
+      error: true,
+      success: false,
+      message: 'Email is already exists',
+    });
   }
-  if(!email) {
-    return res.status(200).jsonp({mailMessage: 'Email is empty'})
+  if (!email) {
+    return res.status(400).jsonp({
+      error: true,
+      success: false,
+      message: 'Email is empty',
+    });
   }
   if (!email.match(constants.correctEmail)) {
-    return res.status(200).jsonp({mailMessage: 'Incorrect email'})
+    return res.status(400).jsonp({
+      error: true,
+      success: false,
+      message: 'Incorrect email',
+    });
   }
-  if(!password) {
-    return res.status(200).jsonp({passwordMessage: 'Password is empty'})
+  if (!password) {
+    return res.status(400).jsonp({
+      error: true,
+      success: false,
+      message: 'Password is empty',
+    });
   }
-  if(password.length < 8) {
-    return res.status(200).jsonp({passwordMessage: 'Password must be more than 8 characters'})
+  if (!password.match(constants.correctPassword)) {
+    return res.status(400).jsonp({
+      error: true,
+      success: false,
+      message: 'Incorrec password',
+    });
   }
-  if(!password.match(constants.correctPassword)) {
-    return res.status(200).jsonp({passwordMessage: 'Incorrect password'})
+
+  const dbCopy = JSON.parse(JSON.stringify(db));
+  const userId = dbCopy.users.length + 1;
+
+  req.body.id = userId;
+
+  db.get('users').push(req.body).write();
+  return res.status(200).jsonp({
+    error: false,
+    success: true,
+    message: 'Registration is successed',
+  });
+});
+
+server.post('/login', (req, res) => {
+  const { db } = req.app;
+  const { password, email } = req.body;
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  const user = db.get('users').find({ email, password }).value() || false;
+
+  if (user) {
+    return res.status(200).jsonp({
+      error: false,
+      success: true,
+      message: 'Login is successed',
+      userData: user,
+    });
   }
 
-  const dbCopy = JSON.parse(JSON.stringify(db))
-  const userId = dbCopy.users.length + 1
+  delete user.password;
+  return res.status(200).jsonp({
+    error: true,
+    success: false,
+    message: 'Email or password incorrect',
+    userData: null,
+  });
+});
 
-  req.body.id = userId
+server.use(middlewares);
 
-  db.get('users').push(req.body).write()
-  // return res.status(200).jsonp(db)
-  next()
-})
+server.use(router);
 
-server.post('/login', (req, res, next) => {
-  const { db } = req.app
-  const {password, email} = req.body
-
-  const isCorrectData = db.get('users').find({email, password}).value()
-
-  if (isCorrectData) {
-    return res.status(200).jsonp({successMessage: 'Login is successed'})
-  } else {
-    return res.status(200).jsonp({errorMessage: 'Email or password arent correct'})
-  }
-})
-
-server.use(middlewares)
-
-server.use(router)
-
-server.listen(3000, () => {
-  console.log('JSON Server is running\nhttp://localhost:3000/')
-})
+server.listen(3001, () => {
+  console.log('JSON Server is running\nhttp://localhost:3001/');
+});
